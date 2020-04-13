c6223edb79863140cef8a3b74ade22c1d6e9cb84
ThatDevCoder <bordia999@gmail.com>
2020-04-14 04:20:15 +0530

    Updates

---
+++ precache-manifest.680f917d7e74dc2e05c639f5f06af677.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5f06b1bdde32c312e11f3ad09a322d5",
+    "url": "/redux-wouldYouRather/index.html"
+  },
+  {
+    "revision": "923913c6e0d9f6c0ccae",
+    "url": "/redux-wouldYouRather/static/css/main.185aa395.chunk.css"
+  },
+  {
+    "revision": "44e144ae28758953a11c",
+    "url": "/redux-wouldYouRather/static/js/2.7fb0b9e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-wouldYouRather/static/js/2.7fb0b9e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "923913c6e0d9f6c0ccae",
+    "url": "/redux-wouldYouRather/static/js/main.4ec99f6e.chunk.js"
+  },
+  {
+    "revision": "69d8d1ed5aacc0e6f243",
+    "url": "/redux-wouldYouRather/static/js/runtime-main.108e806a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/redux-wouldYouRather/static/media/react-logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "fe29b8620e4ad5166d433d3d4e438a75",
+    "url": "/redux-wouldYouRather/static/media/redux-logo.fe29b862.svg"
+  }
+]);
